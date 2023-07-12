24cd1bcbb72cac2d45849130f4450f5dbf2d1382
Ingo Fruend <git@ingofruend.net>
2023-07-12 21:39:22 +0200

    test(super_sum): A few tests

---
+++ tests/test_mypack.py
@@ -0,0 +1,12 @@
+from mypack import super_sum
+
+
+class TestSuperSum:
+    def test_5_and_1_is_6(self):
+        assert super_sum(5, 1) == 6
+
+    def test_2_and_3_is_5(self):
+        assert super_sum(2, 3) == 5
+
+    def test_cummutes(self):
+        assert super_sum(2, 3) == super_sum(3, 2)
